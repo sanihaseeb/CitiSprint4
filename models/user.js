5dc56307bcf44780b742d724d9ac002cee63ab2c
mingdixie <mingdi.xie@mail.utoronto.ca>
2020-08-05 15:06:09 -0400

    1

---
+++ models/user.js
@@ -0,0 +1,23 @@
+/* Restaurant and Reservation Models */
+// DO NOT CHANGE THIS FILE
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 1,
+      trim: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 1
+    }})
+
+// make a model using the User schema
+const User = mongoose.model("User", UserSchema);
+
+module.exports = { User };
