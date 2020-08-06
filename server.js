f7fe47e31af433f664cf418cb7b577104000cbe8
mingdixie <mingdi.xie@mail.utoronto.ca>
2020-08-05 23:46:40 -0400

    add route to get user

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(bodyParser.json());
 const { mongoose } = require('./db/mongoose');
 const { User } = require('./models/user')
 
+// route to add a user
 app.post('/addUser', (req, res) => {
 	const user = new User({
 		name: req.body.name,
@@ -21,6 +22,33 @@ app.post('/addUser', (req, res) => {
 		res.status(400).send(error) // 400 for bad request
 	})
 })
+
+// route to get all users
+app.get("/getUsers", (req, res) => {
+	User.find()
+	  .then(users => {
+		res.send(users);
+	  })
+	  .catch(error => {
+		res.status(500).send(error);
+	  });
+  });
+  
+// route to get a specific user by username
+app.get("/getUser/:userName", (req, res) => {
+	const userName = req.params.userName;
+	User.findOne({ name: userName })
+	  .then(user => {
+		if (!user) {
+			res.status(404).send();
+		} 
+		else {
+			res.send(user)
+		}
+	  })
+	  .catch(error => res.status(500).send);
+  });
+
 
 
 ////////// DO NOT CHANGE THE CODE OR PORT NUMBER BELOW
